294a332287c2aaba23c40813f505c62b265e4e5a
Artyom Zaharov <73885381+Arteww@users.noreply.github.com>
2021-11-27 13:40:49 +0700

    Update diff_equations.py

--- Solutions/6 Differential Equations/diff_equations.py
+++ Solutions/6 Differential Equations/diff_equations.py
@@ -1,5 +1,12 @@
 
-def euler(f, a, b, h):
-    y = []
-    # ...
+def runge(f, a, b, h, y0):
+    y = [y0]
+    i = 0
+    x = a + h
+    while x <= b:
+        i += 1
+        k1 = h * f(x - h, y[i-1])
+        k2 = h * f(x, y[i-1] + k1)
+        y.append(y[i-1] + (k1 + k2) / 2)
+        x += h
     return y
