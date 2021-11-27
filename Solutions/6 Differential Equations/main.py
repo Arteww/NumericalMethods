1cbd8f2dd87d8491f531acf2f1aafd060c9f0b80
Artyom Zaharov <73885381+Arteww@users.noreply.github.com>
2021-11-27 13:40:19 +0700

    Update main.py

--- Solutions/6 Differential Equations/main.py
+++ Solutions/6 Differential Equations/main.py
@@ -1,16 +1,15 @@
-from diff_equations import euler
+from diff_equations import runge
 
 
-# y' = y + x
-def f(x, y): return y + x
+def f(x, y): return 0.0980724253 * (20.0 - y)
 
 
-a, b, h = 0, 1, 0.2
+a, b, h, y0 = 0, 30, 0.2, 100.0
 
-yEuler = euler(f, a, b, h)
+yRunge = runge(f, a, b, h, y0)
 
 # Вывод результата (набор точек).
 x_ = a
-for i in range(len(yEuler)):
-    print(f"({x_}; {yEuler[i]})")
+for i in range(len(yRunge)):
+    print(f"({round(x_,2)}; {round(yRunge[i], 4)})")
     x_ += h
